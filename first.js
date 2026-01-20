3f330901fa7c818e57ebb5e141b575315c35e0de
shethisha1312-jpg <shethisha1312@gmail.com>
2026-01-20 20:24:39 +0530

    lecturer1

---
+++ first.js
@@ -0,0 +1,15 @@
+// const product={
+//     name:"ball pen",
+//     rating:4,
+//     offer:5,
+//     price:270,
+// };
+// console.log(product);
+const profile={
+    username:"isha sheth",
+    isFollow:false,
+    followers:123,
+    following:123,
+
+};
+console.log(typeof profile.username);
