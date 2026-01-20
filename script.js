d83184f125584e105a0c8442c915ac51564382e9
shethisha1312-jpg <shethisha1312@gmail.com>
2026-01-20 20:56:09 +0530

    lecturer-2

---
+++ script.js
@@ -0,0 +1,15 @@
+//Arithmatic operator
+
+let a=5;
+let b=2;
+let c=a+b;
+
+console.log("a=",a,"b=",b);
+console.log("a+b=",a+b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);
+console.log("a%b=",a%b);
+console.log("a**b=",a**b);
+
+
+//unary operator
