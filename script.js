973bd5f532cdda34ba761f329961b8e8d6b11f81
shethisha1312-jpg <shethisha1312@gmail.com>
2026-01-27 20:56:44 +0530

    Array

--- script.js
+++ script.js
@@ -219,5 +219,75 @@
 
 //Template Litrals
 
-let specialString = `this is a template litaral`;
-console.log(typeof    specialString);
+// let specialString = `this is a template litaral`;
+// console.log(typeof    specialString);
+
+// let str = "sheth isha";
+// str.toUpperCase();
+
+// let str ="isha sheth";
+// str = str.toUpperCase();
+// console.log(str);
+
+// let str = "   Isha   ssccs";
+// console.log(str.trim());
+
+// let str = "hello";
+// console.log(str.slice(0,4));
+// let str1="karan";
+// let str2="isha";
+
+// let res=str1    +str2
+// console.log(res);
+
+// let str ="hello";
+// console.log(str.replace("h","y"));
+
+//Q-1
+
+// let fullName=prompt("enter your full name without any space");
+// let userName= "@" +fullName+fullName.length;
+// console.log(userName);  
+
+
+
+//using for loop                      
+// for(let i=0;i< mark.length;i++)
+// {
+//     console.log(mark[i]);
+// }
+
+// //for of
+// for(let el of mark){
+//     console.log(el);
+// }
+
+// let cities = ["delhi","rajkot","mumbai","ahemdabad"];
+// for(let city of cities){
+//     console.log(city.toUpperCase());
+// }
+
+//  let mark=[85,97,44,37,76,60];
+
+//  let sum=0;
+//  for(let val of mark){
+//     sum=sum+val;
+
+//  }
+// console.log(sum);
+// let avg=sum/mark.length;
+// console.log(`avg marks of the calss = ${avg}`);
+
+let items=[250,645,300,900,50];
+let idx=0;
+for (let val of items){
+    console.log(`value at index ${idx}=${val}`);
+    let offer = val/10;
+    items[idx] = items[idx]-offer;
+    console.log(`value after offer= ${items[idx]}`);
+    idx++;
+}
+
+// for(let i=0 ; i<items.length;i++){
+
+// }
